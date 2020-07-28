fa6d8ba135aa95419225fa4a74378da2a8f4c55f
Chacko <chackop@gmail.com>
2020-07-28 17:42:13 +0100

    converted to styled

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,70 +4,51 @@ import styled from 'styled-components';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import {
-  getTodosLoading,
-  getCompletedTodos,
-  getIncompleteTodos,
+    getTodosLoading,
+    getCompletedTodos,
+    getIncompleteTodos,
 } from './selectors';
-import {
-  loadTodos,
-  removeTodoRequest,
-  markTodoAsCompletedRequest,
-} from './thunks';
-import './TodoList.css';
+import { loadTodos, removeTodoRequest, markTodoAsCompletedRequest } from './thunks';
 
-const RedTextComp = styled.div`
-  font-size: 48px;
-  color: red;
+const ListWrapper = styled.div`
+    max-width: 700px;
+    margin: auto;
 `;
 
-const TodoList = ({
-  completedTodos,
-  incompleteTodos,
-  onRemovePressed,
-  onCompletedPressed,
-  isLoading,
-  startLoadingTodos,
-}) => {
-  useEffect(() => {
-    startLoadingTodos();
-  }, []);
-
-  const loadingMessage = <div>Loading todos...</div>;
-  const content = (
-    <div className="list-wrapper">
-      {/* <RedTextComp>This is a red text</RedTextComp> */}
-      <NewTodoForm />
-      <h3>Incomplete:</h3>
-      {incompleteTodos.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          onRemovePressed={onRemovePressed}
-          onCompletedPressed={onCompletedPressed}
-        />
-      ))}
-      <h3>Completed:</h3>
-      {completedTodos.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          onRemovePressed={onRemovePressed}
-          onCompletedPressed={onCompletedPressed}
-        />
-      ))}
-    </div>
-  );
-  return isLoading ? loadingMessage : content;
+const TodoList = ({ completedTodos, incompleteTodos, onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+    
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <ListWrapper>
+            <NewTodoForm />
+            <h3>Incomplete:</h3>
+            {incompleteTodos.map(todo => <TodoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}/>)}
+            <h3>Completed:</h3>
+            {completedTodos.map(todo => <TodoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}/>)}
+        </ListWrapper>
+    );
+    return isLoading ? loadingMessage : content;
 };
 
-const mapStateToProps = (state) => ({
-  isLoading: getTodosLoading(state),
-  completedTodos: getCompletedTodos(state),
-  incompleteTodos: getIncompleteTodos(state),
+const mapStateToProps = state => ({
+    isLoading: getTodosLoading(state),
+    completedTodos: getCompletedTodos(state),
+    incompleteTodos: getIncompleteTodos(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  startLoadingTodos: () => dispatch(loadTodos()),
-  onRemovePressed: (id) => dispatch(removeTodoRequest(id)),
-  onCompletedPressed: (id) => dispatch(markTodoAsCompletedRequest(id)),
+const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
+    onRemovePressed: id => dispatch(removeTodoRequest(id)),
+    onCompletedPressed: id => dispatch(markTodoAsCompletedRequest(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,14 +1,14 @@
 import { createSelector } from 'reselect';
 
-export const getTodos = (state) => state.todos.data;
-export const getTodosLoading = (state) => state.todos.isLoading;
+export const getTodos = state => state.todos.data;
+export const getTodosLoading = state => state.todos.isLoading;
 
 export const getIncompleteTodos = createSelector(
-  getTodos,
-  getTodosLoading,
-  (todos, isLoading) => todos.filter((todo) => !todo.isCompleted)
+    getTodos,
+    (todos) => todos.filter(todo => !todo.isCompleted),
 );
 
-export const getCompletedTodos = createSelector(getTodos, (todos) =>
-  todos.filter((todo) => todo.isCompleted)
+export const getCompletedTodos = createSelector(
+    getTodos,
+    (todos) => todos.filter(todo => todo.isCompleted),
 );
